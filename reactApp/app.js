1c63abad13e86a719c223bceea15133b4eef4483
Julie Wright <wrigjuli@oregonstate.edu>
2018-07-28 21:10:50 -0700

    made list and delete button

--- reactApp/app.js
+++ reactApp/app.js
@@ -1,4 +1,64 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-ReactDOM.render(<p> Swapil is the greatest!!! </p>, document.getElementById('root'));
+const dummyData = [
+  'take my cat for a walk',
+  'make dinner',
+  'create a database',
+  'go to tech meetup',
+  'try on VR headset',
+  'review data structures and algorithms',
+  'learn datascience'
+]
+
+// class InputLine extends React.Component{
+//   constructor(props) {
+//     super(props);
+//   }
+//   render() {
+//     return (
+//
+//     )
+//   }
+// }
+
+class Todo extends React.Component{
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <li> <button>x</button> {this.props.task} </li>
+    )
+  }
+}
+
+class TodoList extends React.Component {
+  constructor(props){
+    super(props);
+  }
+  render() {
+    return (
+      <ul>
+        {dummyData.map((datum)=>{
+          return (
+            <Todo task = {datum} />
+          )
+        })}
+      </ul>
+    )
+  }
+}
+
+// class TodoApp extends React.Component{
+//   constructor(props) {
+//     super(props);
+//   }
+//   render() {
+//     return (
+//
+//     )
+//   }
+// }
+
+ReactDOM.render(<TodoList/>, document.getElementById('root'));
